610a3dd05f61f5304b93aed772ad5e940ae6a014
asthapru <asthaprudigital@gmail.com>
2022-02-17 12:08:28 -0500

    17th Feb Changes

--- Logging/Customlog.sh
+++ Logging/Customlog.sh
@@ -2,4 +2,7 @@
 
 #logger -p local7.info "Info msg"
 
-#trigger 
+#logger -p local7.err "Error Mesage"
+
+#without making any changes to syslog
+logger -s "This is custom messgae"  >> /var/log/CustomMessgae
